fda8612ee2a7032a010fa07fceaebba1cfd45667
Alfatah <alfatah215@gmail.com>
2021-10-10 16:10:31 +0700

    migration  topic_activity

--- migrations/sqls/20211010085741-add-table-topic-activity-up.sql
+++ migrations/sqls/20211010085741-add-table-topic-activity-up.sql
@@ -1,11 +1,11 @@
 /* Replace with your SQL commands */
-CREATE TABLE topic (
+CREATE TABLE topic_activity (
   id INT NOT NULL AUTO_INCREMENT,
   topic_id INT NULL,
-  activity VARCHAR(45) NULL,
-  attachment VARCHAR(45) NULL,
+  activity INT NULL,
+  attachment VARCHAR(200) NULL,
   createdAt DATETIME NULL DEFAULT NOW(),
   createdBy INT NULL,
   updatedAt DATETIME NULL,
-  updatedBy INT NULL
+  updatedBy INT NULL,
   PRIMARY KEY (id));
