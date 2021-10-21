95b7a0784eee5f8f82047f2178180bf321c469f8
Alfatah <alfatah215@gmail.com>
2021-10-21 20:08:14 +0700

    migration create table course attend

---
+++ migrations/sqls/20211021130537-create-table-course-attend-up.sql
@@ -0,0 +1,10 @@
+/* Replace with your SQL commands */
+CREATE TABLE `u1556075_ulms`.`course_attend` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `course_Id` INT NOT NULL,
+  `topic_id` INT NOT NULL,
+  `attendAt` DATETIME NOT NULL DEFAULT NOW(),
+  `status` TINYINT(1) NOT NULL DEFAULT 1,
+  `description` VARCHAR(250) NULL,
+  PRIMARY KEY (`id`));
